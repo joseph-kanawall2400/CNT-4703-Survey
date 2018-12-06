1bd04e127c3f73b0dc813cc9de0c95e7afc497a6
joseph-kanawall2400 <joseph_kanawall2400@daytonastate.edu>
2018-12-05 21:47:26 -0500

    Remove pulling part since that is done manually

--- start.sh
+++ start.sh
@@ -4,11 +4,6 @@ tmux kill-session -t website
 echo Killing Server
 tmux kill-session -t server
 
-if ! [ -n "$1" ]; then
-    echo Pulling
-    git pull
-fi
-
 echo Running Server
 tmux new -d -s website 'sudo dotnet run --project ./Website'
 
